99f2aa48f884c8029ab5a6f163ba908abbf72b3b
nick <nicksanchez2371@gmail.com>
2024-06-10 18:35:59 -0500

    getting error with element not being defined?

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const startButton = document.getElementById('startBtn')
+const nextButton = document.getElementById('nextBtn')
 const questionContainerElement = document.getElementById('questionContainer')
 const questionElement = document.getElementById('question')
 const answerButtonElement = document.getElementById('answerButtons')
@@ -18,15 +19,53 @@ function startGame(){
 }
 
 function setNextQuestion() {
+    resetState()
  showQuestion(shuffleQuestions[currentQuestionsIndex])
 }
 
 function showQuestion(question) {
     questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if(answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonElement.appendChild(button)
+    })
 }
 
-function selectAnswer() {
+function resetState() {
+    nextButton.classList.add('hide')
+    while(answerButtonElement.firstChild) {
+        answerButtonElement.removeChild
+        (answerButtonElement.firstChild)
+    } 
+}
 
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+}
+
+function setStatusClass(element, correct){
+    clearStatusClass(element)
+    if (correct){
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(){
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
 }
 
 const questions = [
